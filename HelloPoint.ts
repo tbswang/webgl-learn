ba022a80a605ca8e4eff479a78bbfe0cb875a35f
gongtao10 <gongtao10@jd.com>
2020-07-12 18:37:32 +0800

    color point

--- HelloPoint.ts
+++ HelloPoint.ts
@@ -5,6 +5,7 @@ import {
 } from './cuon-utils';
 
 export type vec4 = [number, number, number, number];
+export type vec2 = [number, number];
 
 // 定点着色器
 const VSHADER_SOURCE: string = `
@@ -18,12 +19,18 @@ void main(){
 
 // 片段着色器
 const FSHADER_SOURCE: string = `
+precision mediump float;
+uniform vec4 u_FragColor;
 void main(){
-  gl_FragColor=vec4(.0, 1., .0, 1.0);
+  gl_FragColor=u_FragColor;
 }
 `;
 
-const g_points: number[] = [];
+const g_points: Array<vec2> = [];
+const g_colors: Array<vec4> = [];
+const red: vec4 = [1.0, 0.0, 0.0, 1.0];
+const green: vec4 = [0.0, 1.0, 0.0, 1.0] 
+const white: vec4 = [1.0, 1.0, 1.0, 1.0];
 
 let mouseDown = false;
 
@@ -31,29 +38,41 @@ const click = (
   e: MouseEvent,
   gl: WebGL2RenderingContextWithProgram,
   canvas: HTMLCanvasElement,
-  a_Position: number
-) => {
+  a_Position: number,
+  u_FragColor?
+  ) => {
   const { clientX, clientY } = e;
   const rect = (e.target as HTMLElement).getBoundingClientRect();
   // debugger;
   const x = (clientX - rect.x - canvas.width / 2) / (canvas.width / 2);
   const y = (canvas.height / 2 - (clientY - rect.y)) / (canvas.height / 2);
-  g_points.push(x);
-  g_points.push(y);
+  g_points.push([x, y]);
+
+  if(x >= 0.0 && y >= 0.0){
+    // 第一象限
+    g_colors.push(red);
+  } else if ( x < 0.0 && y < 0.0){
+    // 第三象限
+    g_colors.push(green);
+  } else {
+    g_colors.push(white);
+  }
 
   // 注释掉这一行, 点击一下就变成了透明的, 因为默认的颜色分量 是0.0, 就是透明的.
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   const len = g_points.length;
-  for (let i = 0; i < len; i += 2) {
-    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
-
+  for (let i = 0; i < len; i += 1) {
+    const xy = g_points[i];
+    const rgba = g_colors[i];
+    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
     gl.drawArrays(gl.POINTS, 0, 1);
   }
 };
 function main() {
   //
-  const canvas: HTMLCanvasElement = document.getElementById('point');
+  const canvas: HTMLCanvasElement = (document.getElementById('point') as HTMLCanvasElement);
   // const gl = canvas.getContext("webgl");
 
   const gl: WebGL2RenderingContextWithProgram = getWebGLContext(canvas);
@@ -77,18 +96,21 @@ function main() {
   const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
   gl.vertexAttrib1f(a_PointSize, 5.0);
 
+  // 获取点的颜色
+  const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+
+
   canvas.onmousedown = (e) => {
     mouseDown = true;
-    click(e, gl, canvas, a_Position)
+    click(e, gl, canvas, a_Position, u_FragColor)
   };
   // canvas.onmouseup = () => {
   //   mouseDown = false;
   // };
-  canvas.onmousemove = (e) => {
-    if(!mouseDown) return;
-    console.log('canvas.onmousemove -> onmousemove');
-    click(e, gl, canvas, a_Position)
-  };
+  // canvas.onmousemove = (e) => {
+  //   if(!mouseDown) return;
+  //   click(e, gl, canvas, a_Position)
+  // };
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
@@ -100,6 +122,5 @@ function main() {
 main();
 
 document.getElementsByTagName('body')[0].addEventListener('mouseup', () => {
-  console.log('body mouseup');
   mouseDown = false;
 });
