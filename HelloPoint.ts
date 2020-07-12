d0fc619a0cc3efad4cbc1f3c1ec92b65fbd77783
gongtao10 <gongtao10@jd.com>
2020-07-12 15:50:29 +0800

    画画

--- HelloPoint.ts
+++ HelloPoint.ts
@@ -1,6 +1,10 @@
-import { getWebGLContext, initShaders, WebGL2RenderingContextWithProgram } from './cuon-utils';
+import {
+  getWebGLContext,
+  initShaders,
+  WebGL2RenderingContextWithProgram,
+} from './cuon-utils';
 
-export type vec4 = [number, number, number, number]
+export type vec4 = [number, number, number, number];
 
 // 定点着色器
 const VSHADER_SOURCE: string = `
@@ -20,25 +24,32 @@ void main(){
 `;
 
 const g_points: number[] = [];
-const click = (e: MouseEvent, gl: WebGL2RenderingContextWithProgram, canvas: HTMLCanvasElement, a_Position: number ) => {
-  const { clientX, clientY} = e;
+
+let mouseDown = false;
+
+const click = (
+  e: MouseEvent,
+  gl: WebGL2RenderingContextWithProgram,
+  canvas: HTMLCanvasElement,
+  a_Position: number
+) => {
+  const { clientX, clientY } = e;
   const rect = (e.target as HTMLElement).getBoundingClientRect();
   debugger;
-  const x = ((clientX- rect.x) - canvas.width /2) / (canvas.width/2);
-  const y = (canvas.height /2 - (clientY - rect.y)) / (canvas.height /2);
+  const x = (clientX - rect.x - canvas.width / 2) / (canvas.width / 2);
+  const y = (canvas.height / 2 - (clientY - rect.y)) / (canvas.height / 2);
   g_points.push(x);
   g_points.push(y);
 
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   const len = g_points.length;
-  for(let i = 0 ; i< len; i+=2){
-    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], .0);
+  for (let i = 0; i < len; i += 2) {
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
 
     gl.drawArrays(gl.POINTS, 0, 1);
   }
-
-}
+};
 function main() {
   //
   const canvas: HTMLCanvasElement = document.getElementById('point');
@@ -50,22 +61,33 @@ function main() {
     return;
   }
 
-  if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
     console.log('failed to  shader');
     return;
   }
 
-  const a_Position = gl.getAttribLocation(gl.program, 'a_Position' )
-  if(a_Position < 0) {
+  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
     console.error('fail to get position of a_Position');
     return;
   }
-  gl.vertexAttrib3f(a_Position, .5, .5, .0 );
+  gl.vertexAttrib3f(a_Position, 0.5, 0.5, 0.0);
 
   const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
   gl.vertexAttrib1f(a_PointSize, 5.0);
 
-  canvas.onmousedown = (e) => click(e, gl, canvas, a_Position);
+  canvas.onmousedown = (e) => {
+    mouseDown = true;
+    click(e, gl, canvas, a_Position)
+  };
+  // canvas.onmouseup = () => {
+  //   mouseDown = false;
+  // };
+  canvas.onmousemove = (e) => {
+    if(!mouseDown) return;
+    console.log('canvas.onmousemove -> onmousemove');
+    click(e, gl, canvas, a_Position)
+  };
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
@@ -75,3 +97,8 @@ function main() {
 }
 
 main();
+
+document.getElementsByTagName('body')[0].addEventListener('mouseup', () => {
+  console.log('body mouseup');
+  mouseDown = false;
+});
