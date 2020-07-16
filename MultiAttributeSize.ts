e0dfcafdf0c1ea76f840348966ccea4e4fa294ac
gongtao10 <gongtao10@jd.com>
2020-07-16 12:01:59 +0800

    feat:将点的位置和属性放到一个数组中

--- MultiAttributeSize.ts
+++ MultiAttributeSize.ts
@@ -1,5 +1,7 @@
 import {
-  WebGL2RenderingContextWithProgram, getWebGLContext, initShaders,
+  WebGL2RenderingContextWithProgram,
+  getWebGLContext,
+  initShaders,
 } from './cuon-utils';
 import { black } from './common';
 
@@ -20,18 +22,20 @@ void main(){
 `;
 
 function main() {
-  const canvas: HTMLCanvasElement = document.getElementById('point') as HTMLCanvasElement;
+  const canvas: HTMLCanvasElement = document.getElementById(
+    'point'
+  ) as HTMLCanvasElement;
   const gl: WebGL2RenderingContextWithProgram = getWebGLContext(canvas);
-  if(!gl){
+  if (!gl) {
     console.error('fail to init shader');
     return;
   }
-  if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
     console.error('fail to init shader');
     return;
   }
-  const n = initVertexBuffers(gl)
-  if(n < 0){
+  const n = initVertexBuffers(gl);
+  if (n < 0) {
     console.error('fail to set position of vertices');
     return;
   }
@@ -44,21 +48,30 @@ function main() {
 
 function initVertexBuffers(gl: WebGL2RenderingContextWithProgram) {
   const n = 3;
-  
-  const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
-  const vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  const verticesSizes = new Float32Array([
+    0.0,
+    0.5,
+    10.0,
+    -0.5,
+    -0.5,
+    20.0,
+    0.5,
+    -0.5,
+    30.0,
+  ]);
+  const vertexSizeBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW);
+  const FSIZE = verticesSizes.BYTES_PER_ELEMENT;
   const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT,false, 0, 0);
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 3, 0);
   gl.enableVertexAttribArray(a_Position);
-  
-  const sizes = new Float32Array([10.0, 20.0, 30.0]);
-  const sizeBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, sizes, gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW);
   const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
-  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, FSIZE * 3, FSIZE * 2);
   gl.enableVertexAttribArray(a_PointSize);
 
   return n;
