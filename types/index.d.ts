5a6b6a098c96aaac7642bed339f3680bad0e58f0
gongtao10 <gongtao10@jd.com>
2020-07-27 21:37:42 +0800

    ts config

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,15 @@ declare type vec4 = [number, number, number, number];
 
 declare module '*.jpg';
 declare module '*.gif';
+
+declare module '*.vert' {
+  const content: string;
+  // @ts-ignore
+  export default content;
+}
+
+declare module '*.frag' {
+  const content: string;
+  // @ts-ignore
+  export default content;
+}
