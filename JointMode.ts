e9579dc2db2181b753e9f5e90506ef5f8d7c8614
gongtao10 <gongtao10@jd.com>
2020-08-21 11:08:41 +0800

    stash

--- JointMode.ts
+++ JointMode.ts
@@ -1,12 +1,43 @@
-import VERTEXSHADER_SOUCE from './JointMode.vert';
-import FRAGSHADER_SOURCE from './JointMode.frag';
-
 import {
   getWebGLContext,
   initShaders,
   WebGL2RenderingContextWithProgram,
 } from './cuon-utils';
-import { err } from './common';
+import { err, black, initArrayBuffers } from './common';
+import { Matrix4 } from './cuon-matrix';
+
+const VERTEXSHADER_SOUCE: string = `
+#pragma vscode_glsllint_stage: vert
+
+attribute vec4 a_Position;
+attribute vec4 a_Normal;
+uniform mat4 u_MvpMatrix;
+uniform mat4 u_NoramlMatrix;
+varying vec4 v_Color;
+void main(){
+  gl_Position =  u_MvpMatrix * a_Position;
+  vec3 lightDirection = normalize(vec3(0.0, 0.5, 0.7));
+  vec4 color = vec4(1.0, 0.4, 0.0, 1.0);
+  vec3 normal = normalize((u_NoramlMatrix * a_Normal).xyz);
+  float nDotL = max(dot(normal, lightDirection), 0.0);
+  v_Color = vec4(color.rgb * nDotL + vec3(0.1), color.a);
+}
+`;
+
+const FRAGSHADER_SOURCE: string = `
+precision mediump float;
+uniform vec4 v_Color;
+void main(){
+  gl_FragColor = v_Color;
+}
+`;
+
+const g_modelMatrix = new Matrix4(),
+  g_MvpMatrix = new Matrix4();
+const ANGLE_STEP = 3.0;
+const g_arm1Angle = -90;
+const g_jointAngle = 0;
+const g_normalMatrix = new Matrix4();
 
 function main() {
   const canvas: HTMLCanvasElement = document.querySelector('canvas#jointmode');
@@ -19,6 +50,281 @@ function main() {
     err('fail to init shader');
     return;
   }
+  // 设置点的数据
+  const n = initVertexBuffer(gl);
+  if (n < 0) {
+    err('fail to init vertex');
+    return;
+  }
+
+  gl.clearColor(...black);
+  gl.enable(gl.DEPTH_TEST);
+
+  const u_MvpMatrix: WebGLUniformLocation = gl.getUniformLocation(
+    gl.program,
+    'u_MvpMatrix'
+  );
+  const u_NoramlMatrix: WebGLUniformLocation = gl.getUniformLocation(
+    gl.program,
+    'u_NoramlMatrix'
+  );
+  if (!u_MvpMatrix || !u_NoramlMatrix) {
+    err('fail to get location');
+    return;
+  }
+
+  const viewProjMatrix = new Matrix4();
+  viewProjMatrix.setPerspective(50, canvas.width / canvas.height, 1.0, 100);
+  viewProjMatrix.lookAt(20, 10, 30, 0, 0, 0, 0, 1, 0);
+
+  draw(gl, n, viewProjMatrix, u_MvpMatrix, u_NoramlMatrix);
+}
+
+function initVertexBuffer(gl: WebGL2RenderingContextWithProgram) {
+  // Vertex coordinates（a cuboid 3.0 in width, 10.0 in height, and 3.0 in length with its origin at the center of its bottom)
+  let vertices = new Float32Array([
+    1.5,
+    10.0,
+    1.5,
+    -1.5,
+    10.0,
+    1.5,
+    -1.5,
+    0.0,
+    1.5,
+    1.5,
+    0.0,
+    1.5, // v0-v1-v2-v3 front
+    1.5,
+    10.0,
+    1.5,
+    1.5,
+    0.0,
+    1.5,
+    1.5,
+    0.0,
+    -1.5,
+    1.5,
+    10.0,
+    -1.5, // v0-v3-v4-v5 right
+    1.5,
+    10.0,
+    1.5,
+    1.5,
+    10.0,
+    -1.5,
+    -1.5,
+    10.0,
+    -1.5,
+    -1.5,
+    10.0,
+    1.5, // v0-v5-v6-v1 up
+    -1.5,
+    10.0,
+    1.5,
+    -1.5,
+    10.0,
+    -1.5,
+    -1.5,
+    0.0,
+    -1.5,
+    -1.5,
+    0.0,
+    1.5, // v1-v6-v7-v2 left
+    -1.5,
+    0.0,
+    -1.5,
+    1.5,
+    0.0,
+    -1.5,
+    1.5,
+    0.0,
+    1.5,
+    -1.5,
+    0.0,
+    1.5, // v7-v4-v3-v2 down
+    1.5,
+    0.0,
+    -1.5,
+    -1.5,
+    0.0,
+    -1.5,
+    -1.5,
+    10.0,
+    -1.5,
+    1.5,
+    10.0,
+    -1.5, // v4-v7-v6-v5 back
+  ]);
+
+  // Normal
+  let normals = new Float32Array([
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0, // v0-v1-v2-v3 front
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0, // v0-v3-v4-v5 right
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0, // v0-v5-v6-v1 up
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    0.0,
+    0.0, // v1-v6-v7-v2 left
+    0.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    0.0, // v7-v4-v3-v2 down
+    0.0,
+    0.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0, // v4-v7-v6-v5 back
+  ]);
+
+  // Indices of the vertices
+  let indices = new Uint8Array([
+    0,
+    1,
+    2,
+    0,
+    2,
+    3, // front
+    4,
+    5,
+    6,
+    4,
+    6,
+    7, // right
+    8,
+    9,
+    10,
+    8,
+    10,
+    11, // up
+    12,
+    13,
+    14,
+    12,
+    14,
+    15, // left
+    16,
+    17,
+    18,
+    16,
+    18,
+    19, // down
+    20,
+    21,
+    22,
+    20,
+    22,
+    23, // back
+  ]);
+
+  if (!initArrayBuffers(gl, 'a_Position', vertices, gl.FLOAT, 3)) return -1;
+  if (!initArrayBuffers(gl, 'a_Normal', normals, gl.FLOAT, 3)) return -1;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  const indexBuffer = gl.createBuffer();
+  if (!indexBuffer) {
+    console.log('fail to init indexBuffer');
+    return -1; 
+  }
+  gl.bindBuffer(gl.ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  return indices.length;
+}
+
+function draw(
+  gl: WebGL2RenderingContextWithProgram,
+  n,
+  viewProjMatrix: Matrix4,
+  u_MvpMatrix: WebGLUniformLocation,
+  u_NoramlMatrix: WebGLUniformLocation
+) {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  const arm1Length = 10;
+  g_modelMatrix.setTranslate(0, -12, 0);
+  g_modelMatrix.rotate(g_arm1Angle, 0, 1, 0);
+  drawBox(gl, n, viewProjMatrix, u_MvpMatrix, u_NoramlMatrix);
+
+  g_modelMatrix.translate(0, arm1Length, 0);
+  g_modelMatrix.rotate(g_jointAngle, 0, 0, 1);
+  g_modelMatrix.scale(1.3, 1.0, 1.3);
+  drawBox(gl, n, viewProjMatrix, u_MvpMatrix, u_NoramlMatrix);
+}
+
+function drawBox(
+  gl: WebGL2RenderingContextWithProgram,
+  n,
+  viewProjMatrix: Matrix4,
+  u_MvpMatrix: WebGLUniformLocation,
+  u_NoramlMatrix: WebGLUniformLocation
+) {
+  g_MvpMatrix.set(viewProjMatrix);
+  g_MvpMatrix.multiply(g_modelMatrix);
+  gl.uniformMatrix4fv(u_MvpMatrix, false, g_MvpMatrix.elements);
+
+  g_normalMatrix.setInverseOf(g_modelMatrix);
+  g_normalMatrix.transpose();
+  gl.uniformMatrix4fv(u_NoramlMatrix, false, g_normalMatrix.elements);
+
+  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
 }
 
 main();
--- common.ts
+++ common.ts
@@ -1,3 +1,5 @@
+import { WebGL2RenderingContextWithProgram } from './cuon-utils';
+
 export const black: vec4 = [0.0, 0.0, 0.0, 1.0];
 
 export const COLOR = {
@@ -10,8 +12,8 @@ export function getRandomArbitrary(min: number, max: number): number {
   return Math.random() * (max - min) + min;
 }
 
-export function err(msg: string) {
-  console.error(msg);
+export function err(...params: string[]) {
+  console.error(params);
 }
 
 export enum KEY_CODE {
@@ -21,10 +23,36 @@ export enum KEY_CODE {
   down = 40,
 }
 
-export function ifErr(target: any, msg: string): Boolean{
-  if(!target){
+export function ifErr(target: any, msg: string): Boolean {
+  if (!target) {
     err(msg);
     return true;
   }
   return false;
+}
+
+export function initArrayBuffers(
+  gl: WebGL2RenderingContextWithProgram,
+  attribute: string,
+  data: Float32Array,
+  type: number,
+  num: number,
+) {
+  const buffer = gl.createBuffer();
+  if (!buffer) {
+    err('fail to init buffer');
+    return false;
+  }
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+  const a_attribute: GLint = gl.getAttribLocation(gl.program, attribute);
+  if (a_attribute < 0) {
+    err('fail to get attribue', attribute);
+    return false;
+  }
+
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  gl.enableVertexAttribArray(a_attribute);
+  return true;
 }
